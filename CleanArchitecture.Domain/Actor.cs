5d4307e9ed0204162a0dae95c94448a10913f08c
JOHN CARLOS ARRIETA ARRIETA <johncarlosarrietaarrieta@gmail.com>
2024-05-13 22:54:04 -0500

    Crear la capa de  Modelos del negocio

---
+++ CleanArchitecture.Domain/Actor.cs
@@ -0,0 +1,18 @@
+﻿
+using MyProject.Domain.Common;
+
+namespace MyProject.Domain;
+
+public class Actor : BaseDomainModel
+{
+    public Actor() { 
+        Videos = new HashSet<Video>();
+    }
+    
+    public string? Nombre { get; set; }
+
+    public string? Apellido { get; set; }
+
+    public virtual ICollection<Video> Videos { get; set; }
+
+}
---
+++ CleanArchitecture.Domain/Streamer.cs
@@ -0,0 +1,13 @@
+﻿using MyProject.Domain.Common;
+
+namespace MyProject.Domain;
+
+public class Streamer : BaseDomainModel
+{
+    public string? Nombre { get; set; }
+
+    public string? Url { get; set; }
+
+    public ICollection<Video>? Videos { get; set; }
+
+}
