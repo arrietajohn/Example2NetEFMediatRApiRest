621dfc19c1c703c84b851cee21cd99594b6d9982
JOHN CARLOS ARRIETA ARRIETA <johncarlosarrietaarrieta@gmail.com>
2024-05-13 22:40:56 -0500

    Crear la capa del infraestructura y Datos

---
+++ CleanArchitecture.Data/InfrastructureServiceRegistrationExtension.cs
@@ -0,0 +1,38 @@
+﻿using MyProject.Application.Contracts.Infrastructure;
+using MyProject.Application.Contracts.Persistence;
+using MyProject.Application.Models;
+using MyProject.Infrastructure.Email;
+using MyProject.Infrastructure.Persistence;
+using MyProject.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Infrastructure;
+
+public static class InfrastructureServiceRegistrationExtension
+
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services.AddDbContext<StreamerDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("ConnectionString"))
+        );
+
+        services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+        services.AddScoped<IVideoRepository, VideoRepository>();
+        services.AddScoped<IStreamerRepository, StreamerRepository>();
+
+        services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailService, EmailService>();
+
+        return services;
+    }
+
+}
---
+++ CleanArchitecture.Data/Repositories/StreamerRepository.cs
@@ -0,0 +1,11 @@
+﻿using MyProject.Application.Contracts.Persistence;
+using MyProject.Domain;
+using MyProject.Infrastructure.Persistence;
+
+namespace MyProject.Infrastructure.Repositories;
+
+public class StreamerRepository : RepositoryBase<Streamer>, IStreamerRepository
+{
+    public StreamerRepository(StreamerDbContext context) : base(context)
+    { }
+}
